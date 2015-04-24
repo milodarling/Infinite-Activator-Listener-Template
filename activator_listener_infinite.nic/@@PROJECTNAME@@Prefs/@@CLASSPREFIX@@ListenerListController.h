f8e5f6cb068b4cc434192769733a62edfec7180f
milodarling <milodarling98+othergithub@gmail.com>
2015-04-24 06:14:57 -0700

    Added preferences headers

--- activator_listener_infinite.nic/@@PROJECTNAME@@Prefs/@@CLASSPREFIX@@ListenerListController.h
+++ activator_listener_infinite.nic/@@PROJECTNAME@@Prefs/@@CLASSPREFIX@@ListenerListController.h
@@ -1,6 +1,6 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
-#import <Preferences/PSTableCellType.h>
+#import "Preferences/PSListController.h"
+#import "Preferences/PSSpecifier.h"
+#import "Preferences/PSTableCellType.h"
 #import <Cephei/HBPreferences.h>
 #import <objc/runtime.h>
 
--- activator_listener_infinite.nic/@@PROJECTNAME@@Prefs/@@CLASSPREFIX@@RootListController.h
+++ activator_listener_infinite.nic/@@PROJECTNAME@@Prefs/@@CLASSPREFIX@@RootListController.h
@@ -1,7 +1,7 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSEditableListController.h>
-#import <Preferences/PSSpecifier.h>
-#import <Preferences/PSTableCellType.h>
+#import "Preferences/PSListController.h"
+#import "Preferences/PSEditableListController.h"
+#import "Preferences/PSSpecifier.h"
+#import "Preferences/PSTableCellType.h"
 #import <Cephei/HBPreferences.h>
 #import <objc/runtime.h>
 
